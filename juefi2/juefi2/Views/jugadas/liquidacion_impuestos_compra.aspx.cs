707d767a954cc8e0616180b6f158f9290055b71f
jairorojas95 <38729518+jairorojas95@users.noreply.github.com>
2018-05-08 16:24:43 -0500

    impusestos de compra
    la vista de la simulacion de impuesto de un compra

---
+++ juefi2/juefi2/Views/jugadas/liquidacion_impuestos_compra.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace juefi2.Views.jugadas
+{
+    public partial class liquidacion_impuestos_compra : System.Web.UI.Page
+    {
+        double valorcompra;
+        double iva;
+        double reteica;
+        double retefuente;
+        double reteiva;
+        double valorneto;
+
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void guardar_Click(object sender, EventArgs e)
+        {
+
+            valorcompra = Convert.ToDouble(TextBox1.Text);
+
+
+              calcularimouestos( valorcompra);
+
+
+
+
+
+        }
+
+        public void calcularimouestos(double valorcompra) {
+
+            iva = valorcompra * 0.19;
+
+            reteica = (valorcompra * 4) / 1000;
+
+
+            if (valorcompra>=895212) {
+                retefuente = valorcompra * 0.025;
+                reteiva = iva * 0.15;
+
+            }
+
+
+            
+            valorneto = ((valorcompra + iva) - reteiva) - retefuente - reteica;
+            
+
+            TextBox2.Text = Convert.ToString( iva);
+            TextBox3.Text = Convert.ToString(reteica);
+            TextBox4.Text = Convert.ToString(retefuente);
+            TextBox6.Text = Convert.ToString(reteiva);
+            TextBox5.Text = Convert.ToString(valorneto);
+
+        }
+
+
+    }
+}
