763107a73e11de42628f3262e4fc459ce542e23f
jairorojas95 <38729518+jairorojas95@users.noreply.github.com>
2018-05-14 15:14:54 -0500

    jugadas
    juagadas

---
+++ juefi2/juefi2/Views/jugadas/almacenamiento_producto_terminado.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/pagina_maestra/paginamaestra.Master" AutoEventWireup="true" CodeBehind="almacenamiento_producto_terminado.aspx.cs" Inherits="juefi2.Views.jugadas.almacenamiento_producto_terminado" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="col-md-9 ">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Gasto de Almacenamiento de Producto Terminado</h3>
+            </div>
+
+            <div class="box-body">
+                <div class="row">
+                                                       
+                     <div class="col-md-4 col-md-offset-1">
+                        <div class="form-group">
+                            <label>Cantidad</label>
+                            <asp:TextBox ID="TextBox1" runat="server" CssClass=" form-control"></asp:TextBox>
+                        </div>
+                    </div>
+
+
+                 
+                    <div class="col-md-4  col-md-offset-1 ">
+                        <div class="form-group">
+                            <div class="box-footer">
+                                <asp:Button ID="guardar" runat="server" Text="Realizar Almacenamiento" CssClass="btn btn-primary" />
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
---
+++ juefi2/juefi2/Views/jugadas/venta_maquinaria_equipos.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace juefi2.Views.jugadas {
+    
+    
+    public partial class venta_maquinaria_equipos {
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control CheckBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox3;
+        
+        /// <summary>
+        /// Control CheckBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox4;
+        
+        /// <summary>
+        /// Control guardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button guardar;
+    }
+}
