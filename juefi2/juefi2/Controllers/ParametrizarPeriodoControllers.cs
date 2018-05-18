6a57bf1947469c924f577c0a72033bc88f6b1483
jairorojas95 <38729518+jairorojas95@users.noreply.github.com>
2018-05-17 23:48:10 -0500

    parametrizaciones del juego
    parametrizaciones por año y periodo

---
+++ juefi2/juefi2/Controllers/ParametrizarPeriodoControllers.cs
@@ -0,0 +1,22 @@
+﻿using juefi2.Models.BD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace juefi2.Controllers
+{
+    public class ParametrizarPeriodoControllers
+    {
+        parametrizacionPeriodoModel paraperi = new parametrizacionPeriodoModel();
+        public bool actualizar(int id, double valor)
+        {
+
+            paraperi.actualizarvariable(id, valor);
+            return true;
+        }
+
+
+
+    }
+}
---
+++ juefi2/juefi2/Views/parametrizacion/paramtreiacion_año.aspx.designer.cs
@@ -0,0 +1,87 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace juefi2.Views.parametrizacion {
+    
+    
+    public partial class paramtreiacion_variables {
+        
+        /// <summary>
+        /// Control txtuvt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtuvt;
+        
+        /// <summary>
+        /// Control txtea.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtea;
+        
+        /// <summary>
+        /// Control txtdtf.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtdtf;
+        
+        /// <summary>
+        /// Control txtsalrio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtsalrio;
+        
+        /// <summary>
+        /// Control txtrtfuente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtrtfuente;
+        
+        /// <summary>
+        /// Control txtrtica.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtrtica;
+        
+        /// <summary>
+        /// Control txtiva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtiva;
+        
+        /// <summary>
+        /// Control btnguardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnguardar;
+    }
+}
